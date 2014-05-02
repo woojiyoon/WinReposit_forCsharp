5bd1bd759424c957a28f14ff401dd50a29b70a6f
Woo Ji Yoon <wootekken@naver.com>
2014-05-02 13:04:45 +0900

    week7

--- week7/Class4.cs
+++ week7/Class4.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 class test
 {
-    static void printLottery(List<int> n)
+    static void printLottery(List<int> n) // 로또 출력함수
     { 
-        // code here
+  
         Console.Write("Today's lotter number : {0} {1} {2} {3} {4} {5}", n[0], n[1], n[2], n[3], n[4], n[5]);
     }
 
-    static void printList(List<int> n)
+    static void printList(List<int> n) // 셔플로 무작위 섞은 배열을 출력
     {
         foreach (int i in n)
             Console.Write(i + " ");
         Console.WriteLine();
     }
 
-    static void swap(List<int> n, int pos1, int pos2)
+    static void swap(List<int> n, int pos1, int pos2) // 스왑 함수
     {
         int v1 = n.ElementAt(pos1); // posotion of random x
         int v2 = n.ElementAt(pos2); // posotion of end of element
@@ -37,26 +37,17 @@ class test
         for (int i = 1; i <=45; i++)
             LL.Add(i);
 
-      //  printList(LL);
-  //      Random r = new Random();
-    //    int x = r.Next(1, LL.Count - i);
-      //  Console.WriteLine();
-      //  Console.WriteLine(LL.Count);  
-
-        Random r = new Random();
-        //for(int i = 0; i < LL.Count; i++)
+        
+        // 셔플 방법을 이용하여 무작위로 섞는 부분
+	Random r = new Random();
+        
         for (int i = LL.Count; i >= 1; i--)
         {
-            
-            //x = r.Next(1, LL.Count - i);
             x = r.Next(1, i + 1) - 1; 
-            //Console.WriteLine("{0} : {1}", x,LL.Count() - i);
-
-            //swap(LL, x, LL.Count - i- 1);
             swap(LL, x, i - 1);
         }            
-        //swap(LL, 1, 9);
-        printList(LL);
+       
+        printList(LL);  // 출력
         printLottery(LL);
     }
 }
