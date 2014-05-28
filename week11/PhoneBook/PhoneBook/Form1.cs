3a4ce0cfa89b105b889a4d6366f8e06b7b3d5d5a
Woo Ji Yoon <wootekken@naver.com>
2014-05-28 20:18:28 +0900

    week11

---
+++ week11/PhoneBook/PhoneBook/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;// for io
+using System.Xml.Serialization; // for seralization
+using System.Xml;
+
+namespace PhoneBook
+{
+    public partial class Form1 : Form
+    {
+        PhoneBook pb;
+        public Form1()
+        {
+            InitializeComponent();
+            pb = new PhoneBook();
+            //pb.Add("우재우", "없음", "1학기솔로");
+            //pb.Add("김중일", "031-222-4444", "교수");
+            //pb.Add("홍길동", "011-123-1234", "도둑");
+ 
+            saveFileDialog1.Filter = "저장파일(*.xml)|*.xml|전부(*.*)|*.*";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+
+            if (!(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK))
+                return;
+            StreamReader sr = new StreamReader(openFileDialog1.FileName,Encoding.GetEncoding("euc-kr"));
+
+            XmlSerializer xs = new XmlSerializer(typeof(PhoneBook));//PhoneBook을 시리얼라이즈 할거야.
+
+            //XmlReader rd = XmlReader.Create(sr);
+
+            pb = (PhoneBook)xs.Deserialize(sr);
+
+            sr.Close();
+
+            textBox1.Text = pb.getAllData();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!(saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK))
+                return;
+
+            StreamWriter sw = new StreamWriter(saveFileDialog1.FileName, false, Encoding.GetEncoding("euc-kr"));
+            //sw.WriteLine(textBox1.Text);
+            
+            XmlSerializer xs = new XmlSerializer(typeof(PhoneBook));//PhoneBook을 시리얼라이즈 할거야.
+            xs.Serialize(sw, pb); // txt파일화하기
+            sw.Close();
+        }
+    }
+}
---
+++ week11/PhoneBook/PhoneBook/PhoneData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class PhoneBook
+    {
+        public List <PhoneData> phoneList;
+
+        public string getAllData()
+        { 
+            string ret = "";
+            foreach(PhoneData p in phoneList)
+            {
+                ret += string.Format("{0}||{1}||{2}\r\n", p.mGroup, p.mName, p.mPhone); 
+            }
+            return ret;
+        }
+        public PhoneBook()
+        {
+            phoneList = new List<PhoneData>();
+        }
+
+        public void Add(string name, string phone, string group)
+        {
+            PhoneData p = new PhoneData(name, phone, group);
+            phoneList.Add(p);
+        }
+
+        public PhoneData FindPeople(string name)
+        {
+            foreach (PhoneData p in phoneList)
+            {
+                if (p.mName == name)
+                    return p;
+            }
+            return null;
+        }
+    }
+
+    public class PhoneData
+    {
+        public string mName;
+        public string mPhone;
+        public string mGroup;
+
+        public PhoneData()
+        {
+            mName = "John Doe";
+            mPhone = "010-1234-5678";
+            mGroup = "None";
+        }
+
+        public PhoneData(string name, string phone, string group)
+        {
+            mName = name;
+            mPhone = phone;
+            mGroup = group;
+        }
+        
+    }
+}
