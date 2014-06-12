b87c2b24ec329d5cef87a22ae476196cc15ee1fd
Woo Ji Yoon <wootekken@naver.com>
2014-06-12 12:33:47 +0900

    2014_spring_finalproject
    0612

---
+++ TypingABC/TypingABC/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TypingABC
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rtxSentence = new System.Windows.Forms.RichTextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName1 = new System.Windows.Forms.Label();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblName2 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblName3 = new System.Windows.Forms.Label();
+            this.lblCorrect = new System.Windows.Forms.Label();
+            this.panBoard = new System.Windows.Forms.Panel();
+            this.tmTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panBoard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtxSentence
+            // 
+            this.rtxSentence.Location = new System.Drawing.Point(0, 3);
+            this.rtxSentence.Multiline = false;
+            this.rtxSentence.Name = "rtxSentence";
+            this.rtxSentence.ReadOnly = true;
+            this.rtxSentence.Size = new System.Drawing.Size(560, 185);
+            this.rtxSentence.TabIndex = 0;
+            this.rtxSentence.Text = "";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(3, 218);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(560, 21);
+            this.txtInput.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(246, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(38, 12);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "label1";
+            // 
+            // lblName1
+            // 
+            this.lblName1.AutoSize = true;
+            this.lblName1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblName1.Location = new System.Drawing.Point(53, 312);
+            this.lblName1.Name = "lblName1";
+            this.lblName1.Size = new System.Drawing.Size(38, 12);
+            this.lblName1.TabIndex = 3;
+            this.lblName1.Text = "label1";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(113, 312);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(38, 12);
+            this.lblSpeed.TabIndex = 4;
+            this.lblSpeed.Text = "label2";
+            // 
+            // lblName2
+            // 
+            this.lblName2.AutoSize = true;
+            this.lblName2.Location = new System.Drawing.Point(246, 312);
+            this.lblName2.Name = "lblName2";
+            this.lblName2.Size = new System.Drawing.Size(38, 12);
+            this.lblName2.TabIndex = 5;
+            this.lblName2.Text = "label3";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(300, 312);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(38, 12);
+            this.lblAverage.TabIndex = 6;
+            this.lblAverage.Text = "label4";
+            // 
+            // lblName3
+            // 
+            this.lblName3.AutoSize = true;
+            this.lblName3.Location = new System.Drawing.Point(412, 312);
+            this.lblName3.Name = "lblName3";
+            this.lblName3.Size = new System.Drawing.Size(38, 12);
+            this.lblName3.TabIndex = 7;
+            this.lblName3.Text = "label5";
+            // 
+            // lblCorrect
+            // 
+            this.lblCorrect.AutoSize = true;
+            this.lblCorrect.Location = new System.Drawing.Point(491, 312);
+            this.lblCorrect.Name = "lblCorrect";
+            this.lblCorrect.Size = new System.Drawing.Size(38, 12);
+            this.lblCorrect.TabIndex = 8;
+            this.lblCorrect.Text = "label6";
+            // 
+            // panBoard
+            // 
+            this.panBoard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panBoard.Controls.Add(this.rtxSentence);
+            this.panBoard.Controls.Add(this.txtInput);
+            this.panBoard.Location = new System.Drawing.Point(12, 30);
+            this.panBoard.Name = "panBoard";
+            this.panBoard.Size = new System.Drawing.Size(570, 257);
+            this.panBoard.TabIndex = 9;
+            // 
+            // tmTimer
+            // 
+            this.tmTimer.Interval = 1000;
+            this.tmTimer.Tick += new System.EventHandler(this.tmTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(561, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "산성비게임설정버튼 영역";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(613, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "난이도 상 중 하";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 356);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panBoard);
+            this.Controls.Add(this.lblCorrect);
+            this.Controls.Add(this.lblName3);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblName2);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.lblName1);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "frmMain";
+            this.Text = "Speed Up";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panBoard.ResumeLayout(false);
+            this.panBoard.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxSentence;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName1;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblName2;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblName3;
+        private System.Windows.Forms.Label lblCorrect;
+        private System.Windows.Forms.Panel panBoard;
+        private System.Windows.Forms.Timer tmTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TypingABC/TypingABC/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypingABC
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        const int BACKSPACE_KYE = 8; // backspace key의 아스키 코드값 8
+        const int ENTER_KEY = 13; // enter key 아스키 코드 값 13
+        const int SPACE_KEY = 32; // space key 아스키 코드 값 32
+
+        int m_nIndex = 0; // 글자 위치(사용자의 입력 횟수를 의미함)
+        int m_nLen = 0; // 현재 보이는 문장의 길이
+        String m_strFile; // 파일 경로
+        System.IO.StreamReader m_file;  // 파일 내용을 얻기 위한 객체
+
+        int m_nTime; // 사용자가 타이핑하는 데 걸린 시간
+        int m_nTotalSpeed; // 타이핑 스피드를 모두 더하기
+        int m_nTry; // 문장 테스트 횟수
+        int m_nCorrect; // 정확도 표시
+
+        // 폼을 로드함과 동시에 파일도 로드, 타이핑 초기화 준비
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // 파일 열기
+            m_strFile = Application.StartupPath + "\\txt\\TypingTest.txt";
+
+            try
+            {
+                m_file = System.IO.File.OpenText(m_strFile);
+            }
+            catch
+            {
+                MessageBox.Show(m_strFile + "File Open Failed.");
+                this.Close();
+                return;
+            }
+
+            // 프로그램 시작과 동시에 원문을 보여주기 위해 한 라인 읽기
+            rtxSentence.Text = NextSentence(); // 다음 문장 가져오기(미완성)
+            m_nLen = rtxSentence.Text.Length; // 현재 보이는 문장의 길이 넣기
+
+            m_nTry = 1; // 시도횟수
+            tmTimer.Interval = 1000;
+
+            Init();
+        }
+
+        // 데이터 초기화
+        private void Init() {
+            m_nIndex = 0; // 글자위치
+            txtInput.Clear(); // 사용자 입력 텍스트박스 지우기
+            tmTimer.Stop();
+            m_nTime = 0; // 사용자가 타이핑 하는데 걸린 시간 초기화
+        }
+
+        //파일에서 다음 문장을 읽어서 리턴해준다.
+        private String NextSentence() {
+            String str;
+            str = m_file.ReadLine();
+
+            if (str == null) { // 읽을 내용이 없으면 첨 부터 다시 읽기
+                m_file.Close();
+                m_file = null;
+
+                try
+                {
+                    m_file = System.IO.File.OpenText(m_strFile);
+                }
+                catch {
+                    MessageBox.Show(m_strFile + "Failed File Open");
+                    this.Close();
+                    return null;
+                }
+                str = m_file.ReadLine();
+            }
+            // 타이핑 결과 속도나 정확도 같은거 설정하는 부분 추가해야함
+
+            m_nLen = str.Length;
+
+            // 다음 문장의 색깔을 검은색으로 되돌릴 코드 추가해야함
+
+            return str;
+        }
+
+        // 사용자의 타이핑한 시간 설정
+        private void tmTimer_Tick(object sender, EventArgs e)
+        {
+            m_nTime += 1;
+        }
+        //************************************//
+        // 현재 상황 :
+        // 윈도우 폼 작성 완료
+        // 텍스트 파일 불러오기
+        // 사용자가 글자 타이핑 하기
+        //**********************************//
+        // 추가할 메소드들 :
+        // 틀린 타이핑 처리
+        // 엔터키 처리
+        // 글자 지우기 처리
+        // 사용자의 타이핑 속도, 정확도 표시
+        // 시간이 된다면 산성비 게임 까지 !
+        // **********************************//
+
+       
+    }
+}
